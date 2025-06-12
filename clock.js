31b9f15f13e64281d6bb9554aa12d50bdf160dd3
alfajr <165387645+ALFAJ14287@users.noreply.github.com>
2025-06-12 16:51:29 +0100

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,1 +1,22 @@
+const  hours=document.querySelector('.hours');
+const  minutes=document.querySelector('.minutes');
+const  seconds=document.querySelector('.seconds');
+const  am=document.querySelector('.am');
+ 
+updateClock();
+function updateClock(){
+  let h=new Date().getHours();
+  let m=new Date().getMinutes();
+  let s=new Date().getSeconds();
+  let am='AM';
+  if (h >12){
+    h=h-12;
+    am='AM'
+  }
+  hours.innerHTML=`${h}`
+  minutes.innerHTML=`${m}`
+  seconds.innerHTML=`${s}`
+  am.innerHTML=am;
+  
+}
 
